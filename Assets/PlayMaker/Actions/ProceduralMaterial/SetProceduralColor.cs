07f3977a32107667ab9cdc2a98762ef96b2e74d9
DESKTOP-FVSCF23\htw75 <htw750128@gmail.com>
2018-05-11 07:57:20 +0800

    #修正PlayMaler錯誤

--- Assets/PlayMaker/Actions/ProceduralMaterial/SetProceduralColor.cs
+++ Assets/PlayMaker/Actions/ProceduralMaterial/SetProceduralColor.cs
@@ -55,7 +55,7 @@ namespace HutongGames.PlayMaker.Actions
 
 	    private void DoSetProceduralFloat()
         {
-#if !(UNITY_IPHONE || UNITY_IOS || UNITY_ANDROID || UNITY_NACL || UNITY_FLASH || UNITY_PS3 || UNITY_PS4 || UNITY_XBOXONE || UNITY_BLACKBERRY || UNITY_METRO || UNITY_WP8 || UNITY_WIIU || UNITY_PSM || UNITY_WEBGL || UNITY_SWITCH)
+#if !(UNITY_2018_1_OR_NEWER || UNITY_IPHONE || UNITY_IOS || UNITY_ANDROID || UNITY_NACL || UNITY_FLASH || UNITY_PS3 || UNITY_PS4 || UNITY_XBOXONE || UNITY_BLACKBERRY || UNITY_METRO || UNITY_WP8 || UNITY_WIIU || UNITY_PSM || UNITY_WEBGL || UNITY_SWITCH)
 
             var substance = substanceMaterial.Value as ProceduralMaterial;
 			if (substance == null)
